dfe99f9b0ba8bcf0fd972f9e495119a8f045d32e
Thirumalesh25 <229x1a0540@gprec.ac.in>
2025-03-20 05:18:23 +0530

    Update Lexical_Analyzer.java

--- CD(LAB)/Lexical_Analyzer.java
+++ CD(LAB)/Lexical_Analyzer.java
@@ -1,48 +1,29 @@
 import java.util.*;
 
-public class Lexical_Analyzer {
-
-    public static boolean isKeyword(String str) {
-        Set<String> key = new HashSet<>(Arrays.asList(
-        "if","else","while","do","break","continue","int","void"));
-        return key.contains(str);
+class Lexical_Analyzer{
+    static void print(String m){
+        System.out.println(m);
     }
-
-    public static void print(String message) {
-        System.out.println(message);
-    }
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        sc.close();
-
-        int s = 0;
-
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-
-            if (";,+-/*=<>(){}[] ".contains(ch + "")) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("LineOfCode : ");
+        String str=sc.nextLine();
+        HashSet<String> key =new HashSet<>(Arrays.asList("if", "else", "int","break"));
+        
+        int s=0;
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(";,+-*=/><(){}[] ".contains(ch+"")){
                 String tk = str.substring(s, i).trim();
-
-                if (!tk.isEmpty()) {
-                    if (isKeyword(tk)) {
-                        print("KEYWORD: " + tk);
-                    } else if (Character.isDigit(tk.charAt(0))) {
-                        print("INVALID: " + tk);
-                    } else {
-                        print("IDENTIFIER: " + tk);
-                    }
+                if(!tk.isEmpty()){
+                    if(key.contains(tk)) print("KEYWORD : "+tk);
+                    else if(Character.isDigit(tk.charAt(0))) print("INVALID : "+tk);
+                    else print("IDENTIFIER : "+tk);
                 }
-
-                s = i + 1;
-                if (ch == ' ') continue;
-
-                if ("+-*/><=".contains(ch + "")) {
-                    print("OPERATOR: " + ch);
-                } else {
-                    print("DELIMITER: " + ch);
-                }
+                s=i+1;
+                if(ch==' ') continue;
+                if("+-*=></".contains(ch+"")) print("OPERATOR : "+ch);
+                else print("DELIMITER : "+ch);
             }
         }
     }
