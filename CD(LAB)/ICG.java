a39bcbe8233ed9953d31bfefcb3e1dcf15d8b5e6
Thirumalesh25 <229x1a0540@gprec.ac.in>
2025-03-20 07:41:18 +0530

    Update ICG.java

--- CD(LAB)/ICG.java
+++ CD(LAB)/ICG.java
@@ -1,66 +1,47 @@
 import java.util.*;
 
-public class ICG {
-
-    static int val(char c) {
-    switch (c) {
-        case '+':
-        case '-':
-            return 1;
-        case '*':
-        case '/':
-            return 2;
-        case '^':
-            return 3;
-        case '(':
-            return 4;
-        default:
-            return -1;
+class ICG {
+    static int val(char ch){
+        switch(ch){
+            case '+':
+            case '-': return 1;
+            case '*':
+            case '/': return 2;
+            case '^': return 3;
+            case '(' :return 4;
+            default : return -1;
+        }
     }
-}
-
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
         System.out.print("Arithmetic Exp : ");
-        String exp = sc.next();
-        String ans = "";
-
-        Stack<Character> s = new Stack<>();
-
-        for (int i = 0; i < exp.length(); i++) {
-            char ch = exp.charAt(i);
-
-            if (val(ch) == -1) {
-                ans += ch;
-            } else if (s.isEmpty() || val(ch) > val(s.peek())) {
-                s.push(ch);
-            } else {
-                while (!s.isEmpty() && val(ch) <= val(s.peek())) {
-                    ans += s.pop();
+        String exp=sc.nextLine();
+        String ans="";
+        Stack<Character> s1=new Stack<>();
+        for(int i=0;i<exp.length();i++){
+            char ch=exp.charAt(i);
+            if(val(ch)==-1) ans+=ch;
+            else if(s1.isEmpty() || val(ch)>val(s1.peek())) s1.push(ch);
+            else{
+                while(!s1.isEmpty() && val(ch) <=val(s1.peek())){
+                    ans+=s1.pop();
                 }
-                s.push(ch);
+                s1.push(ch);
             }
         }
-
-        while (!s.isEmpty()) {
-            ans += s.pop();
-        }
-
-        Stack<String> s1 = new Stack<>();
-        int j = 0;
-
-        for (int i = 0; i < ans.length(); i++) {
-            char ch = ans.charAt(i);
-
-            if (val(ch) != -1) {
-                String l1 = s1.pop();
-                String l2 = s1.pop();
-                System.out.println("t" + j + " = " + l2 + ch + l1);
-                s1.push("t" + j);
+        while(!s1.isEmpty()) ans+=s1.pop();
+        
+        int j=0;
+        Stack<String> s2 =new Stack<>();
+        for(int i=0;i<ans.length();i++){
+            char ch=ans.charAt(i);
+            if(val(ch)==-1) s2.push(ch+"");
+            else{
+                String o2=s2.pop();
+                String o1=s2.pop();
+                System.out.println("t"+j+"="+o1+ch+o2);
+                s2.push("t"+j);
                 j++;
-            } else {
-                s1.push(ch + "");
             }
         }
     }
